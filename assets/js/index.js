ad84b75893c0c5faf13837a423e7c1b0ec70b7a8
Anish Shrestha <me.anish.shrestha@gmail.com>
2022-07-06 17:34:25 +0545

    track maker done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ let gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
 let gradient2 = ctx.createLinearGradient(0, 0, canvas.width, 0);
 let inputElement = document.getElementById("number");
 let mainMenuElement = document.getElementById("main_menu");
+let trackEditorPage = document.getElementById("track_editor_page");
 
 let errorMessageElement = document.createElement("div");
 errorMessageElement.id = "error_message";
@@ -96,7 +97,6 @@ function playerDead(player) {
 }
 
 async function saveData({ id, life, score, track }) {
-  console.log(id, life, score);
   fetch("https://contrabackend.herokuapp.com/addScore", {
     method: "POST",
     headers: {
@@ -115,7 +115,6 @@ async function saveData({ id, life, score, track }) {
       showMessage("successMessage", "Saved");
     })
     .catch((err) => {
-      console.log(err);
       showMessage("errorMessage", "Error on saving");
     });
 }
@@ -125,7 +124,6 @@ class Gameplay {
     //for saving game
     saveBtn.style.display = "block";
     saveBtn.addEventListener("click", () => {
-      console.log(this.players[0].life, this.players[0].score, playTrack, inputElement.value);
       saveData({ life: this.players[0].life, score: this.players[0].score, track: playTrack, id: inputElement.value });
     });
     this.backgroundImage = new Background();
@@ -147,7 +145,6 @@ class Gameplay {
     }
 
     if (gameResource) {
-      console.log("game resource", JSON.parse(gameResource.track));
       this.players[0].score = gameResource.score;
       this.players[0].life = gameResource.life;
       trackArray = JSON.parse(gameResource.track);
@@ -490,6 +487,7 @@ function mainMenu() {
   canvas.style.display = "none";
   let singlePlayerBtn = document.getElementById("single_player_btn");
   let doublePlayerBtn = document.getElementById("double_player_btn");
+  let trackEditorBtn = document.getElementById("track_editor");
   let continueBtn = document.getElementById("continue_btn");
 
   continueBtn.addEventListener("click", async () => {
@@ -530,6 +528,167 @@ function mainMenu() {
       startGame(2);
     }
   });
+  trackEditorBtn.addEventListener("click", () => {
+    if (inputElement.value == "") {
+      showMessage("errorMessage", "Empty number");
+    } else {
+      gameOverElement.style.display = "none";
+      gameWinElement.style.display = "none";
+      mainMenuElement.style.display = "none";
+      displayTrackEditor();
+      // startGame(2);
+    }
+  });
+}
+function displayTrackEditor() {
+  let selectedId = 1;
+
+  let body = document.body;
+  let gridContainer = document.createElement("div");
+  gridContainer.height = "100vh";
+  body.appendChild(gridContainer);
+  gridContainer.style.position = "absolute";
+  gridContainer.style.top = "0px";
+  gridContainer.style.left = "0px";
+
+  //for selecting the list of items
+
+  let grass = document.createElement("img");
+  grass.style.height = "30px";
+  grass.style.width = "30px";
+  grass.src = "/assets/images/2.png";
+  grass.style.position = "absolute";
+  grass.style.bottom = "-50px";
+  grass.style.left = "0px";
+
+  grass.addEventListener("click", () => {
+    selectedId = 2;
+  });
+
+  gridContainer.appendChild(grass);
+
+  let track = document.createElement("img");
+  track.style.height = "30px";
+  track.style.width = "30px";
+  track.src = "/assets/images/1.png";
+  track.style.position = "absolute";
+  track.style.bottom = "-50px";
+  track.style.left = "30px";
+
+  track.addEventListener("click", () => {
+    selectedId = 1;
+  });
+
+  gridContainer.appendChild(track);
+
+  let wall = document.createElement("img");
+  wall.style.height = "30px";
+  wall.style.width = "30px";
+  wall.src = "/assets/images/9.png";
+  wall.style.position = "absolute";
+  wall.style.bottom = "-50px";
+  wall.style.left = "120px";
+
+  wall.addEventListener("click", () => {
+    selectedId = 9;
+  });
+
+  gridContainer.appendChild(wall);
+
+  let enemyBot = document.createElement("img");
+  enemyBot.style.height = "30px";
+  enemyBot.style.width = "30px";
+  enemyBot.src = "/assets/images/6.png";
+  enemyBot.style.position = "absolute";
+  enemyBot.style.bottom = "-50px";
+  enemyBot.style.left = "180px";
+
+  enemyBot.addEventListener("click", () => {
+    selectedId = 6;
+  });
+
+  gridContainer.appendChild(enemyBot);
+
+  let bridge = document.createElement("img");
+  bridge.style.height = "30px";
+  bridge.style.width = "30px";
+  bridge.src = "/assets/images/4.png";
+  bridge.style.position = "absolute";
+  bridge.style.bottom = "-50px";
+  bridge.style.left = "240px";
+
+  bridge.addEventListener("click", () => {
+    selectedId = 4;
+  });
+
+  gridContainer.appendChild(bridge);
+
+  let bridgeStart = document.createElement("img");
+  bridgeStart.style.height = "30px";
+  bridgeStart.style.width = "30px";
+  bridgeStart.src = "/assets/images/3.png";
+  bridgeStart.style.position = "absolute";
+  bridgeStart.style.bottom = "-50px";
+  bridgeStart.style.left = "300px";
+
+  bridgeStart.addEventListener("click", () => {
+    selectedId = 3;
+  });
+
+  gridContainer.appendChild(bridgeStart);
+
+  let bridgeEnd = document.createElement("img");
+  bridgeEnd.style.height = "30px";
+  bridgeEnd.style.width = "30px";
+  bridgeEnd.src = "/assets/images/5.png";
+  bridgeEnd.style.position = "absolute";
+  bridgeEnd.style.bottom = "-50px";
+  bridgeEnd.style.left = "360px";
+
+  bridgeEnd.addEventListener("click", () => {
+    selectedId = 5;
+  });
+
+  gridContainer.appendChild(bridgeEnd);
+
+  let array = [];
+
+  for (let y = 0; y < 9; y++) {
+    let arr = [];
+    array.push(arr);
+    for (let x = 0; x < 40; x++) {
+      let imageElement = document.createElement("img");
+      imageElement.style.height = "30px";
+      imageElement.style.width = "30px";
+      imageElement.style.border = "2px solid black";
+      imageElement.style.display = "inline";
+
+      imageElement.name = `{"y":${y},"x":${x}}`;
+      imageElement.addEventListener("click", (event) => {
+        event.target.src = `./assets/images/${selectedId}.png`;
+        let { x, y } = JSON.parse(event.target.name);
+        array[y][x] = selectedId;
+      });
+      gridContainer.appendChild(imageElement);
+      arr.push(0);
+    }
+    gridContainer.appendChild(document.createElement("br"));
+  }
+  let playBtn = document.createElement("button");
+  playBtn.innerText = "Play";
+  playBtn.style.padding = "10px 20px";
+  playBtn.style.borderRadius = "25px";
+  playBtn.style.position = "absolute";
+  playBtn.style.bottom = "-50px";
+  playBtn.style.right = "0px";
+  playBtn.addEventListener("click", () => {
+    gridContainer.style.display = "none";
+    canvas.style.display = "block";
+
+    let data = { score: 0, life: 4, track: JSON.stringify(array) };
+    startGame(1, data);
+  });
+  gridContainer.appendChild(playBtn);
 }
 
 mainMenu();
